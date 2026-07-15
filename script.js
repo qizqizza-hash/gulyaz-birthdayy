d5de2152d228cf9c7627a243c9ee8153530c22a0
qizqizza-hash <qizqizza@gmail.com>
2026-07-15 17:24:05 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-// Momo hələ danışmır 😊
+function nextScene() {
+
+    document.getElementById("scene1").classList.add("hidden");
+    document.getElementById("scene2").classList.remove("hidden");
+
+}
+
+function yesIam() {
+
+    alert("Əla! 💖 İndi macəra başlayır...");
+
+}
